693f35c00f34aa21dce7bad5271cb0807f386396
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-11-27 20:44:10 -0500

    Se modifica el modelo del chat

--- backend/src/main/java/com/chatapp/model/Chat.java
+++ backend/src/main/java/com/chatapp/model/Chat.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DBRef;
+//import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.time.LocalDateTime;
 import java.util.List;
