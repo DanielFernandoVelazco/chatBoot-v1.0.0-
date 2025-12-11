0589cd1770df919c191f08ec7770dfad216f8fbd
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-12-11 11:04:48 -0500

    Se actualiza routing app

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
+import { NoAuthGuard } from './core/guards/no-auth.guard';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        loadChildren: () => import('./modules/chat/chat.module').then(m => m.ChatModule)
+    },
     {
         path: 'auth',
+        canActivate: [NoAuthGuard],
         loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
     },
-    // Agregaremos más rutas después
+    {
+        path: 'chat',
+        canActivate: [AuthGuard],
+        loadChildren: () => import('./modules/chat/chat.module').then(m => m.ChatModule)
+    },
+    {
+        path: 'settings',
+        canActivate: [AuthGuard],
+        loadChildren: () => import('./modules/settings/settings.module').then(m => m.SettingsModule)
+    },
+    { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
