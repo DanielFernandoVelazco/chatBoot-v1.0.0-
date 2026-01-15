7030a4c2f5dcf323cfd478eb495c511e74098393
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-01-14 20:38:42 -0500

    Se reinicia la logica del repo

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'https://tu-dominio.com/api',
-    wsUrl: 'https://tu-dominio.com/ws'
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8081/api',
-    wsUrl: 'http://localhost:8081/ws'
-};
