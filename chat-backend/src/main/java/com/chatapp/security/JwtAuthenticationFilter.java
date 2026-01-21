9cfbd0812a216ce28e08af6f463462800685f87e
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-01-20 23:31:58 -0500

    Se normaliza security filter

--- chat-backend/src/main/java/com/chatapp/security/JwtAuthenticationFilter.java
+++ chat-backend/src/main/java/com/chatapp/security/JwtAuthenticationFilter.java
@@ -24,6 +24,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
+
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
