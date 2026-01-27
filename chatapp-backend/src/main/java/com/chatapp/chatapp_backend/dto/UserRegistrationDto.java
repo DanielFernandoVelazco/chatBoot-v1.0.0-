1282e3945e876004589ec83049a1e43a3d08a1ba
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-01-26 21:05:05 -0500

    Se normaliza dto registration

---
+++ chatapp-backend/src/main/java/com/chatapp/chatapp_backend/dto/UserRegistrationDto.java
@@ -0,0 +1,25 @@
+package com.chatapp.chatapp_backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRegistrationDto {
+
+    @NotBlank(message = "El nombre de usuario es obligatorio")
+    private String username;
+
+    @Email(message = "El formato del email no es válido")
+    @NotBlank(message = "El email es obligatorio")
+    private String email;
+
+    @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
+    @NotBlank(message = "La contraseña es obligatoria")
+    private String password;
+}
