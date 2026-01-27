e8d7bdf79baff2c01123ed7031fb550b0f54bdf5
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-01-26 21:00:04 -0500

    Se normaliza user repository user

---
+++ chatapp-backend/src/main/java/com/chatapp/chatapp_backend/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.chatapp.chatapp_backend.repository;
+
+import com.chatapp.chatapp_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Método para buscar usuario por su nombre de usuario
+    Optional<User> findByUsername(String username);
+
+    // Método para buscar usuario por email (útil para el login)
+    Optional<User> findByEmail(String email);
+
+    // Verificar si existe el email
+    Boolean existsByEmail(String email);
+}
