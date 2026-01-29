e6ac951e2977825766cc61ff88319ef8219bdf38
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-01-28 21:42:07 -0500

    Se normaliza service user it

--- chatapp-backend/src/main/java/com/chatapp/chatapp_backend/service/UserService.java
+++ chatapp-backend/src/main/java/com/chatapp/chatapp_backend/service/UserService.java
@@ -3,10 +3,14 @@ package com.chatapp.chatapp_backend.service;
 import com.chatapp.chatapp_backend.dto.UserRegistrationDto;
 import com.chatapp.chatapp_backend.dto.UserResponseDto;
 
+import java.util.List;
+
 public interface UserService {
     UserResponseDto registerUser(UserRegistrationDto registrationDto);
     UserResponseDto findByEmail(String email);
 
     // AGREGAR ESTA LÍNEA:
     UserResponseDto loginUser(String email, String rawPassword);
+
+    List<UserResponseDto> getAllUsers();
 }
