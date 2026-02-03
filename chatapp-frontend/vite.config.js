a174da16e6504dfd2dee1d70c81b731c9f64bba1
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-02-02 21:20:16 -0500

    Se normaliza vite

--- chatapp-frontend/vite.config.js
+++ chatapp-frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Esta línea soluciona el error de "global is not defined" en SockJS
+    global: 'globalThis',
+  },
 })
