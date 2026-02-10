e5604beb3f177f9e0711a41a966cbdffb2a6209f
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-02-09 23:32:33 -0500

    Se normaliza dto contact request

---
+++ chatapp-backend/src/main/java/com/chatapp/chatapp_backend/dto/ContactRequestDto.java
@@ -0,0 +1,32 @@
+package com.chatapp.chatapp_backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContactRequestDto {
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
+    private String name;
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "El formato del email no es válido")
+    private String email;
+
+    @NotBlank(message = "El asunto es obligatorio")
+    @Size(min = 5, max = 200, message = "El asunto debe tener entre 5 y 200 caracteres")
+    private String subject;
+
+    @NotBlank(message = "El mensaje es obligatorio")
+    @Size(min = 10, max = 2000, message = "El mensaje debe tener entre 10 y 2000 caracteres")
+    private String message;
+
+    private Long userId; // Opcional, si el usuario está autenticado
+}
